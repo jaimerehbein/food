01148bd877ce5a692eef96f6fa7436ba2a6f6186
Jaime Rehbein <rebbeim7@gmail.com>
2017-11-20 05:25:47 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MenuComponent } from './menu/menu.component';
+import { BodyComponent } from './body/body.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MenuComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule
